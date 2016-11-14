58176bd026f1d97cedbae72dafd587be8147e42d
Ihor Popkov <ihor.popkov@gmail.com>
2016-11-14 23:39:23 +0200

    Add Word Model

---
+++ Orionik.EnglishTextsTrainer.Models/Word.cs
@@ -0,0 +1,11 @@
+﻿namespace Orionik.EnglishTextsTrainer.Models
+{
+    public class Word
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Meanings { get; set; }
+        public bool Ignore { get; set; }
+        public bool Know { get; set; }
+    }
+}
