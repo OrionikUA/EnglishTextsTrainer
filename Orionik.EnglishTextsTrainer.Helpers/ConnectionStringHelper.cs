02f549cdd494c2803f94f136a448bb16c656fa43
Ihor Popkov <ihor.popkov@gmail.com>
2016-11-15 20:16:46 +0200

    Add Helpers functionality

---
+++ Orionik.EnglishTextsTrainer.Helpers/ConnectionStringHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orionik.EnglishTextsTrainer.Helpers
+{
+    public class ConnectionStringHelper
+    {
+        public readonly String Connection;
+        private static ConnectionStringHelper _instance;
+
+        private ConnectionStringHelper() { Connection = ConfigurationManager.ConnectionStrings["EnglishTextsTrainerConnectionString"].ConnectionString; }
+
+        public static ConnectionStringHelper Instance => _instance ?? (_instance = new ConnectionStringHelper());
+
+    }
+}
