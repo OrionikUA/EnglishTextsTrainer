cf5effc6dd1cd25bd2aa4a4685528b1d990ea339
Ihor Popkov <ihor.popkov@gmail.com>
2016-11-15 10:01:39 +0200

    Change Logic functionality

---
+++ Orionik.EnglishTextsTrainer.Logic/TextFile.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Orionik.EnglishTextsTrainer.Logger;
+
+namespace Orionik.EnglishTextsTrainer.Logic
+{
+    public static class TextFile
+    {
+        public static string ReadFile(string path)
+        {
+            Logging.Instance.Write(typeof(TextFile), "Start ReadFile");
+            var stringBuilder = new StringBuilder();
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        stringBuilder.Append(line);
+                        stringBuilder.Append(" ");
+                    }
+                }
+                
+            }
+            else
+            {
+                var exception = new FileNotFoundException("Cannot open file", path);
+                Logging.Instance.Write(typeof(TextFile), exception);
+                throw exception;
+            }
+            Logging.Instance.Write(typeof(TextFile), "End ReadFile");
+            return stringBuilder.ToString().ToLower();
+        }
+
+        public static void WriteToFile(string text, string filePath)
+        {
+            Logging.Instance.Write(typeof(TextFile), "Start WriteToFile overload string");
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+            using (FileStream fs = File.Create(filePath))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes(text);
+                fs.Write(info, 0, info.Length);
+            }
+            Logging.Instance.Write(typeof(TextFile), "End WriteToFile overload string");
+        }
+
+        public static void WriteToFile(List<string> list, string filePath)
+        {
+            Logging.Instance.Write(typeof(TextFile), "Start WriteToFile overload list");
+            var stringBuilder = new StringBuilder();
+            foreach (var word in list)
+            {
+                stringBuilder.Append(word);
+                stringBuilder.Append("\n");
+            }
+            WriteToFile(stringBuilder.ToString(), filePath);
+            Logging.Instance.Write(typeof(TextFile), "End WriteToFile overload list");
+        }
+    }
+}
---
+++ Orionik.EnglishTextsTrainer.Logic/TextWords.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Orionik.EnglishTextsTrainer.Logger;
+
+namespace Orionik.EnglishTextsTrainer.Logic
+{
+    public static class TextWords
+    {
+        public static List<string> FindWords(string text)
+        {
+            Logging.Instance.Write(typeof(TextWords), "Start FindWords");
+            var pattern = new Regex(
+                @"( [^\W_\d]( [^\W_\d] | [-'\d](?=[^\W_\d]))*[^\W_\d] )",
+                RegexOptions.IgnorePatternWhitespace);
+            Logging.Instance.Write(typeof(TextWords), "End FindWords");
+            return (from Match match in pattern.Matches(text) select match.Groups[1].Value).ToList();
+
+        }
+
+        public static List<string> GetUniqueWordList(List<string> list)
+        {
+            Logging.Instance.Write(typeof(TextWords), "Start GetUniqueWordList");
+            var wordList = new List<string>();
+
+            foreach (var item in wordList)
+            {
+                if (!wordList.Contains(item))
+                {
+                    wordList.Add(item);
+                }
+            }
+            Logging.Instance.Write(typeof(TextWords), "End GetUniqueWordList");
+            return wordList;
+        }
+
+        public static Dictionary<string, int> GetUniqueWordDictionary(List<string> list)
+        {
+            Logging.Instance.Write(typeof(TextWords), "Start GetUniqueWordDictionary");
+            var wordDictionary = new Dictionary<string, int>();
+
+            foreach (var item in list)
+            {
+                if (wordDictionary.ContainsKey(item))
+                {
+                    wordDictionary[item]++;
+                }
+                else
+                {
+                    wordDictionary.Add(item, 1);
+                }
+            }
+            Logging.Instance.Write(typeof(TextWords), "End GetUniqueWordDictionary");
+            return wordDictionary;
+        }
+    }
+}
