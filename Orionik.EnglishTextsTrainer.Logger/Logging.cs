f3bce034edfc18953e508bf80a9142ecb77c945d
Ihor Popkov <ihor.popkov@gmail.com>
2016-11-15 10:12:01 +0200

    Change Logger functionality

--- Orionik.EnglishTextsTrainer.Logger/Logging.cs
+++ Orionik.EnglishTextsTrainer.Logger/Logging.cs
@@ -4,12 +4,16 @@ using System.IO;
 
 namespace Orionik.EnglishTextsTrainer.Logger
 {
+    public delegate void WriteToComponent(string text);
+
     public class Logging
     {
         public static Logging Instance => _instance ?? (_instance = new Logging());
 
         private static Logging _instance;
         private readonly string _logFile;
+
+        public WriteToComponent WriteDelegate { get; set; }
 
         private Logging()
         {
@@ -26,18 +30,23 @@ namespace Orionik.EnglishTextsTrainer.Logger
             }
         }
 
+        private void Write(string message)
+        {
+            Debug.WriteLine(message);
+            WriteIntoFile(message);
+            WriteDelegate(message);
+        }
+
         public void Write(Type type, string message)
         {
             var logMessage = $"{DateTime.Now:yyyy-MM-dd (HH:mm:ss)} - {type}: {message}";
-            Debug.WriteLine(logMessage);
-            WriteIntoFile(logMessage);
+            Write(logMessage);
         }
 
         public void Write(Type type, Exception exception)
         {
             var logMessage = $"Exception! {DateTime.Now:yyyy-MM-dd (HH:mm:ss)} - {type}: {exception.Message}";
-            Debug.WriteLine(logMessage);
-            WriteIntoFile(logMessage);
+            Write(logMessage);
         }
 
         private void WriteIntoFile(string message)
