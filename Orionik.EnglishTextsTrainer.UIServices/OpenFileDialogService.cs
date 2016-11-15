3d7a18fb1d0320e345945d5945db968246ef682c
Ihor Popkov <ihor.popkov@gmail.com>
2016-11-15 20:17:30 +0200

    Add UIService functionality

---
+++ Orionik.EnglishTextsTrainer.UIServices/OpenFileDialogService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace Orionik.EnglishTextsTrainer.UIServices
+{
+    public class OpenFileDialogService
+    {
+        private readonly string _defaultExtension;
+        private readonly string _filter;
+
+        public OpenFileDialogService(string defaultExtension, string filter)
+        {
+            _defaultExtension = defaultExtension;
+            _filter = filter;
+        }
+
+        public string OpenFile()
+        {
+            var fd = new OpenFileDialog
+            {
+                DefaultExt = _defaultExtension,
+                Filter = _filter
+            };
+            var result = fd.ShowDialog() ?? false;
+            return result ? fd.FileName : null;
+        }
+    }
+}
