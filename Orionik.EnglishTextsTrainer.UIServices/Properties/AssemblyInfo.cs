f0626c18a2d2a3e7b6fcf5bacdc991192b50c677
Ihor Popkov <ihor.popkov@gmail.com>
2016-11-15 17:25:38 +0200

    UIServices Settings

---
+++ Orionik.EnglishTextsTrainer.UIServices/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("Orionik.EnglishTextsTrainer.UIServices")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Orionik.EnglishTextsTrainer.UIServices")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9e451dac-e29b-4fb0-b6f8-86e7dddaca6a")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
