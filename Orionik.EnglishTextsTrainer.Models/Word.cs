0a3edc2d850f143d68cca909ef5023bb45d74274
Ihor Popkov <ihor.popkov@gmail.com>
2016-11-16 18:07:58 +0200

    Change Models

--- Orionik.EnglishTextsTrainer.Models/Word.cs
+++ Orionik.EnglishTextsTrainer.Models/Word.cs
@@ -7,5 +7,10 @@
         public string Meanings { get; set; }
         public bool Ignore { get; set; }
         public bool Know { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}|{Meanings}";
+        }
     }
 }
