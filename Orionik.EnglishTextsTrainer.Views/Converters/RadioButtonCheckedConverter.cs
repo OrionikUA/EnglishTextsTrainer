5b88d3d759992759c3a186865091debec02e295b
Ihor Popkov <ihor.popkov@gmail.com>
2016-11-16 17:29:20 +0200

    Change Views

---
+++ Orionik.EnglishTextsTrainer.Views/Converters/RadioButtonCheckedConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Orionik.EnglishTextsTrainer.ViewModels;
+
+namespace Orionik.EnglishTextsTrainer.Views.Converters
+{
+    public class RadioButtonCheckedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            return ((Filter)value).Equals((Filter)parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            return value.Equals(true) ? parameter : Binding.DoNothing;
+        }
+    }
+}
